7c5b8455f7040b55532fea98e11262008d5306aa
wolfbian <746053931@qq.com>
2016-08-20 17:07:59 +0800

    feat : 定义了几个需要的接口

---
+++ frontend/controllers/GameController.php
@@ -0,0 +1,57 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+
+/**
+ * Game controller
+ */
+class GameController extends JsonBaseController{
+
+    /**
+     * @name  卡牌移动
+     * @method POST
+     * @author wolfbian
+     * @date 2016-08-20
+     * @param    int            card_id
+     * @param    int            from
+     * @param    int            to
+     */
+    public function actionMoveCard(){
+
+    }
+
+    /**
+     * @name  洗卡
+     * @method POST
+     * @author wolfbian
+     * @date 2016-08-20
+     * @param    int            type (0：手牌，1：牌库，2：弃牌区，3：死亡牌区)
+     */
+    public function actionShuttleCard(){
+
+    }
+
+    /**
+     * @name  在同区域内移动卡牌
+     * @method POST
+     * @author wolfbian
+     * @date 2016-08-20
+     * @param    array          起始坐标 {x: ***, y: ***}              
+     * @param    array          终点坐标 {x: ***, y: ***}              
+     */
+    public function actionMoveCardInboard(){
+
+    }
+
+    /**
+     * @name  卡牌状态改变
+     * @method POST
+     * @author wolfbian
+     * @date 2016-08-20
+     */
+    public function actionChangeCardState(){
+
+    }
+
+}
---
+++ frontend/controllers/JsonBaseController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\Exception;
+use yii\web\Response;
+use yii\web\Controller;
+
+class JsonBaseController extends Controller{
+
+    public function init(){
+
+        if ($this->request->get('callback')) {
+            Yii::$app->response->format = Response::FORMAT_JSONP;
+        }else{
+            Yii::$app->response->format = Response::FORMAT_JSON;
+        }
+
+        parent::init();
+    }
+
+}
