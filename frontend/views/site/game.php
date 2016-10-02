aeb107ddd4ec6fca25e4960aea77bead38b15f43
wu123456 <746053931@qq.com>
2016-10-02 11:31:49 +0800

    fix: 加入common.css

--- frontend/views/site/game.php
+++ frontend/views/site/game.php
@@ -4,9 +4,11 @@
 
 use yii\helpers\Html;
 
+
 $this->title = 'Game';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<link href="/css/common.css" rel="stylesheet">
 <div id="app"></div>
 
 <script src="/js/game.js"></script>
