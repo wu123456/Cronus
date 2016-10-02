fba34a92a689aeecb91999768d9a01b4e85a39b5
wu123456 <746053931@qq.com>
2016-10-03 00:01:02 +0800

    feat ： 前端手牌区

---
+++ frontend/web/js/index-game.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './modules/App'
+import GameBoard from './modules/GameBoard'
+
+render(<GameBoard/>, document.getElementById('app'))
--- frontend/web/js/modules/GameBoard.js
+++ frontend/web/js/modules/GameBoard.js
@@ -101,3 +101,49 @@ class Card extends Component{
 
 
 }
+
+class Board extends Component {
+	render() {
+		return <div><GameBoard/><Hands/></div>;
+	}
+}
+
+class Hands extends Component {
+	constructor(props) {
+		super(props);
+        this.state = {
+            cards: []
+        };
+    }
+
+    render() {
+    	let cards = this.state.cards;
+		return (<div className="game-hands" ref="t"
+					onDrop = {this.handleDrop.bind(this)}
+					onDragOver={this.handDragover.bind(this)}
+					>
+					{cards}
+				</div>);
+    }
+
+    handleDrop(event) {
+		if (moveElement.props.fatherName == this.props.name) {
+			moveElement.setState({x: event.pageX - moveElement._x, y: event.pageY - moveElement._y});
+			return;
+		}
+
+		let a = <Card  key={new Date() - 0} {...moveElement.props} fatherName={this.props.name} x={event.pageX - this.refs.t.offsetLeft - moveElement._x} y={event.pageY - this.refs.t.offsetTop - moveElement._y}/>;
+		
+		this.setState(function(oldState){
+			oldState.cards.push(a);
+			return oldState;
+		});
+
+	}
+
+	handDragover(event) {
+		event.preventDefault()
+	}
+}
+
+export default Board
